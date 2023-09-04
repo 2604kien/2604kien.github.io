d6941d0c76cb80cb161f71c1af92d280f86a4764
DESKTOP-F3BMJTI\nguye <hngu0053@student.monash.edu>
2023-09-04 20:02:16 +1000

    Header component created

--- src/index.js
+++ src/index.js
@@ -4,10 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
-  </React.StrictMode>
 );
 reportWebVitals();
