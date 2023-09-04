b36d93ac41eeedba98fdf5844bb4aea55dc4d57b
tcb2307 <buituancuong44@gmail.com>
2023-09-04 21:21:43 +1000

    created inital nav bar and header section

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,8 +2,10 @@ import React from "react";
 
 export default function Header(){
     return (
-        <div>
-            
+        <div className="container-fluid middle">
+            <h1>Hong Kien Nguyen</h1>
+            <h4>Full Stack Developer</h4>
+            <h2>Welcome to my portfolio</h2>
         </div>
     );
 }
