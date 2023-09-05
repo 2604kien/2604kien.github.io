fe9124639fd08ebe7b89792b40591fa078a02736
DESKTOP-F3BMJTI\nguye <hngu0053@student.monash.edu>
2023-09-05 16:23:09 +1000

    add About section

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import React from 'react';
 import Header from "./Components/Header";
 import Navbar from './Components/Navbar';
+import About from "./Components/About"
 import "../node_modules/bootstrap/dist/css/bootstrap.css";
 function App() {
   return (
     <div className="App">
      <Navbar />
      <Header />
+     <About />
     </div>
   );
 }
---
+++ src/Components/About.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function About(){
+    return(
+        <div className="container-md border border-primary">
+           <div className="row">
+            <div className="col">
+            About me
+            </div>
+            <div className="col">
+                
+            </div>
+           </div>
+        </div>
+    );
+}
