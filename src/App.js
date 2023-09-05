45c65048325050d6f86453698c5f902ffdd8d850
Andy Bui <Andy.Bui@egplusww.com.au>
2023-09-05 15:07:27 +1000

    adding gsap animation library

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import React from 'react';
 import Header from "./Components/Header";
 import Navbar from './Components/Navbar';
 import "../node_modules/bootstrap/dist/css/bootstrap.css";
+
+
 function App() {
   return (
     <div className="App">
