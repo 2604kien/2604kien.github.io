53799c0da604a897118d790f06600428fee2317e
Hong Kien Nguyen <nguyenhongkien123tk@gmail.com>
2023-09-07 04:04:08 +1000

    finish animation about me section

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,25 +1,56 @@
 import React from "react";
 import Profile from "../Images/Profile.jpg";
+import { motion, useViewportScroll, useInView, useAnimation} from "framer-motion/dist/framer-motion"
+import "./About.css"
 import { description } from "../Documents/AboutMe";
 export default function About(){
-
+    const text=React.useRef(null);
+    const imageRef=React.useRef(null);
+    const isTextInView=useInView(text,{once:true});
+    const isImageInView=useInView(imageRef, {once:true});
+    const controlText=useAnimation();
+    const controlImage=useAnimation();
+    React.useEffect(()=>{
+        if(isTextInView) controlText.start("visible");
+        if(isImageInView) controlImage.start("visible");
+    },[isTextInView, isImageInView])
     return(
-        <div className="container-fluid border border-primary ">
-            <div className="row border border-primary ">
-                <h2 className="d-flex justify-content-center">About Me</h2>
+        <div className="container-fluid animateSection">
+            <div className="row">
+                <h2 className="d-flex justify-content-center border-bottom ">About Me</h2>
             </div>
-           <div className="row">
-            <div className="col d-flex align-items-center p-5">
-            <p className="text-center">
+           <div  className="row">
+            <div className="col d-flex align-items-center p-5 animate">
+            
+            <motion.p ref={text}
+            variants={{
+                hidden:{opacity:0, x:-75},
+                visible:{opacity:1, x:0}
+            }}
+            initial="hidden"
+            animate={controlText}
+            transition={{type:"tween", duration:1.5, delay: 0.25}}
+            className="text-center">
                 {description}
-            </p>
+            </motion.p>
+            
             </div>
-            <div className="col border border-primary p-0">
-                <div>
-                    <img className="img-fluid" src={Profile}/>
-                </div>
+            <div className="col p-0 animate">
+            <motion.div ref={imageRef} 
+            whileHover={{scale:1.025}}
+            variants={{
+                hidden:{opacity:0, x:75, scale: 1},
+                visible:{opacity:1, x:0}
+            }}
+            initial="hidden"
+            animate={controlImage}
+            transition={{type:"tween", duration:1}}
+           >
+                    <img className="img-fluid" style={{borderRadius: "15px"}} src={Profile}/>
+            </motion.div>
             </div>
            </div>
+
         </div>
     );
 }
--- src/Documents/AboutMe.js
+++ src/Documents/AboutMe.js
@@ -1,11 +1,4 @@
-export const description=`I am Hong Kien. I just graduated Bachelor of Business and Information Technology,
- majoring in Software Development and Accounting from Monash University. I am well-versed in developing 
- specifications and requirements, designing software applications, and database structuring.I am a passionate
-  and creative problem solver, with high responsibility for the tasks that I am allocated to. During my education,
-   I gain knowledge not only in programming aspects but also in the business aspect, because I believe a developer
-    should also have an understanding of business aspects such as the scope of budget, revenue generation expectation,
-     ...etc. These gave me the technical knowledge and real-world experience required to excel in this role. During my
-      course of study, I demonstrated my creativity and problem-solving skills to deliver sophisticated software solutions.
-       My experience working on my own projects in a team environment has shown my ability to work collaboratively,
-        foster a capacity for creative problem-solving, and take ownership of projects. I am not afraid of challenges
-         or taking on full responsibility for the task and I will always try to learn and improve myself day by day.`;
+export const description=`I am Hong Kien Nguyen. I just graduated Bachelor of Business and Information Technology, 
+majoring in Software Development and Accounting from Monash University. I am well-versed in developing specifications
+ and requirements, designing software applications, and database structuring. I am not afraid of challenges or taking on 
+ full responsibility for the task and I will always try to learn and improve myself day by day.`;
