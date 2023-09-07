f26e7d3cd1a9de3d2b86e9fd8d7104d728b58e68
DESKTOP-F3BMJTI\nguye <hngu0053@student.monash.edu>
2023-09-07 17:16:36 +1000

    change background

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,15 +1,19 @@
 import React from "react";
+import Profile from "../Images/Profile.jpg";
 import { motion, useViewportScroll, useInView, useAnimation} from "framer-motion/dist/framer-motion"
 import "./About.css"
 import { description } from "../Documents/AboutMe";
 export default function About(){
     const text=React.useRef(null);
+    const imageRef=React.useRef(null);
     const isTextInView=useInView(text,{once:true});
+    const isImageInView=useInView(imageRef, {once:true});
     const controlText=useAnimation();
+    const controlImage=useAnimation();
     React.useEffect(()=>{
         if(isTextInView) controlText.start("visible");
-      
-    },[isTextInView])
+        if(isImageInView) controlImage.start("visible");
+    },[isTextInView, isImageInView])
     return(
         <div className="container-fluid animateSection">
             <div className="row">
@@ -31,7 +35,20 @@ export default function About(){
             </motion.p>
             
             </div>
-           
+            <div className="col p-0 animate">
+            <motion.div ref={imageRef} 
+            whileHover={{scale:1.025}}
+            variants={{
+                hidden:{opacity:0, x:75, scale: 1},
+                visible:{opacity:1, x:0}
+            }}
+            initial="hidden"
+            animate={controlImage}
+            transition={{type:"tween", duration:1}}
+           >
+                    <img className="img-fluid" style={{borderRadius: "15px"}} src={Profile}/>
+            </motion.div>
+            </div>
            </div>
 
         </div>
