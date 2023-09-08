f56464460118ff3a25de95f8a7e4545293c6531a
Hong Kien Nguyen <nguyenhongkien123tk@gmail.com>
2023-09-08 12:24:38 +1000

    change in heade

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import Header from "./Components/Header";
 import Navbar from './Components/Navbar';
 import About from "./Components/About"
 import "../node_modules/bootstrap/dist/css/bootstrap.css";
-
+import background from "./Images/HeaderBackground.png"
 
 function App() {
   return (
     <div className="App">
+      <img
+      src={background} style={{position: "fixed", width: "100%", zIndex:"-9"}}/>
      <Navbar />
      <Header />
      <About />
