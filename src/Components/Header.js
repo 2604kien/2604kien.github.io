c7cc6aa243be9a298e9393995020ee21c23acc71
DESKTOP-F3BMJTI\nguye <hngu0053@student.monash.edu>
2023-09-10 04:45:19 +1000

    change header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,21 +1,21 @@
 import React, { useState } from "react";
-import moon from "../Images/moon.svg";
-import {motion} from "framer-motion/dist/framer-motion";
 import "./Header.css";
-import {data} from "../Documents/SignatureSVG"
 import photo from "../Images/HeaderBackground1.png"
+import gsap from "gsap";
 
 export default function Header(){
-
-    
-
-
+    let item1=React.useRef(null);
+    React.useEffect(()=>{
+    item1.style.display="none";
+      
+    },[]);
+    console.log("hi")
     return (
         <div >
           <div className="grid-container" width="100%">
-          <div className="name" style={{fontSize:"5vw", letterSpacing:2}}>KIEN NGUY</div>
+          <div ref={item1} className="name" style={{fontSize:"5vw", letterSpacing:2}}>KIEN NGUY</div>
           <div className="explore" style={{fontSize:"5vw", wordBreak:"break-all", lineHeight: 1, paddingTop: "1.2vw", textAlign:"center"}}>EXPLORE ↓</div>
-          <div className="name2" style={{fontSize:"5vw"}}>N</div>
+          <div className="name2" style={{fontSize:"5vw"}}><div>N</div></div>
           <div className="title" style={{fontSize:"5vw"}}>FULL-STACK DEVELOPER</div>
           <img src={photo} className="intro"/>
           </div>
