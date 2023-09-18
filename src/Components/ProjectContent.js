923f368f66507b0ab0b2faef04eb4779952c442e
Hong Kien Nguyen <nguyenhongkien123tk@gmail.com>
2023-09-19 02:44:39 +1000

    change design for project section

--- src/Components/ProjectContent.js
+++ src/Components/ProjectContent.js
@@ -1,36 +1,35 @@
 import React from "react";
 import "./css/ProjectItem.css";
-import gsap from "gsap";
-import { ScrollTrigger } from "gsap/all";
-import {motion, useAnimation} from "framer-motion/dist/framer-motion"
+
+import {motion, useAnimation, useInView} from "framer-motion/dist/framer-motion"
 export default function ProjectContent(props){
     let component=React.useRef(null);
-    gsap.registerPlugin(ScrollTrigger)
+    let isInView=useInView(component, {once:false});
+    const control=useAnimation();
     React.useEffect(()=>{
-        let ctx= gsap.context(()=>{
-            gsap.from(component,{
-                scrollTrigger: {
-                    trigger: component,
-                    toggleActions: "play none none none"
-                },
-                duration: 1,
-                y:100,
-                opacity: 0,
-                ease: "easeInOut",
-                delay: props.id<3? props.id/5: (props.id-3)/5
-            })
-        });
-        return ()=>{ctx.revert()}
-    },[])
+        if(isInView){
+            control.start("visible");
+        }
+    },[isInView])
     return (
-        <div onClick={()=>{window.location.href=props.link}} ref={el=>{component=el}}  className="projectItem">
-                <h2>{props.name}</h2>
-                <h2 style={{position: "absolute", color: "#333", top:"0%", left:"-5%", opacity: 0.3, fontSize: "70px"}}>0{props.id+1}</h2>
-                
-                <motion.img whileHover={{scale: 1.3}} style={{width: "100%", borderRadius: 20}} src={props.src} />
+        <motion.div 
+        variants={{
+            hidden: {opacity: 0, x: 100},
+            visible: {opacity: 1, x: 0}
+        }}
+        initial="hidden"
+        animate={control}
+        transition={{type:"spring", duration: 2}}
+        onClick={()=>{window.location.href=props.link}} ref={component}  className="projectItem">
+            
+            <div style={{display: "flex", alignItems:"center", flexDirection: "row", width: "fit-content"}}>
+                <h2 style={{color: "#333", opacity: 0.3, fontSize: "50px"}}>0{props.id+1}</h2>
+                <h2 style={{marginLeft:"15px"}}>{props.name}</h2>
+            </div>  
+                <motion.img whileHover={{scale: 1.05}} style={{width: "100%", borderRadius: 20}} src={props.src} />
                
                 <p style={{fontSize: "1.2rem"}}>Skills: {props.skill}</p>
                 <p style={{fontSize: "1.2rem"}}>{props.description}</p>
-        </div>
+        </motion.div>
     )
 }
