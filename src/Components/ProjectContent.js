1cabe7c2541c66b8ce8a36bbc62fbb85349a2c09
Hong Kien Nguyen <nguyenhongkien123tk@gmail.com>
2023-09-24 16:01:47 +1000

    finish portfolio

--- src/Components/ProjectContent.js
+++ src/Components/ProjectContent.js
@@ -1,17 +1,27 @@
 import React from "react";
 import "./css/ProjectItem.css";
-
+import next from "./Images/next.png"
 import {motion, useAnimation, useInView} from "framer-motion/dist/framer-motion"
 export default function ProjectContent(props){
+    const [currIMG, setCurrIMG]=React.useState(0);
     let component=React.useRef(null);
     let isInView=useInView(component, { once: false });
     const control=useAnimation();
-    const[current, setCurrent]=React.useState(0);
     React.useEffect(()=>{
         if(isInView){
             control.start("visible");
         }
-    },[isInView])
+    },[isInView]);
+    const prevImage=()=>{
+        setCurrIMG(prev=>{
+            return prev<=0? props.src.length-1 : prev-1;
+        })
+    }
+    const nextImage=()=>{
+        setCurrIMG(prev=>{
+            return prev>=props.src.length-1? 0 : prev+1;
+        })
+    }
     return (
         <motion.div 
         variants={{
@@ -22,14 +32,17 @@ export default function ProjectContent(props){
         animate={control}
         transition={{type:"spring", duration: 2}}
         ref={component}  className="projectItem">
-            
-            <div style={{display: "flex", alignItems:"center", flexDirection: "row", width: "fit-content"}}>
+            <div style={{display: "flex", alignItems:"center", flexDirection: "row", width: "fit-content",width:"75%", marginLeft:"12.5%"}}>
                 <h2 style={{color: "#333", opacity: 0.3, fontSize: "50px"}}>0{props.id+1}</h2>
                 <h2 style={{marginLeft:"15px"}}>{props.name}</h2>
-            </div>  
-                <motion.img onClick={()=>{window.location.href=props.link}} whileHover={{scale: 1.05}} style={{width: "100%", borderRadius: 20, cursor: "pointer"}} src={props.src} />
-                <p style={{fontSize: "1.2rem"}}><span style={{fontWeight: "600"}}>Skills:</span> {props.skill}</p>
-                <p style={{fontSize: "1.2rem"}}>{props.description}</p>
+            </div>    
+                <div className="image--section">
+                    <img src={next} onClick={prevImage} style={{visibility: props.src.length<=1?"hidden": "visible", cursor: "pointer"}} className="prev"/>
+                    <img className="proIMG" onClick={()=>{window.location.href=props.link}} style={{width: "100%", borderRadius: 20, cursor: "pointer"}} src={props.src[currIMG]} />
+                    <img src={next} onClick={nextImage} style={{visibility: props.src.length<=1?"hidden": "visible", cursor: "pointer"}} className="next" />
+                </div>
+                <p style={{fontSize: "1.2rem",width:"75%", marginLeft:"12.5%"}}><span style={{fontWeight: "600"}}>Skills:</span> {props.skill}</p>
+                <p style={{fontSize: "1.2rem", width:"75%", marginLeft:"12.5%"}}>{props.description}</p>
         </motion.div>
     )
 }
