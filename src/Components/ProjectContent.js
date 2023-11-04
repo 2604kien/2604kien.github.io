7e6cc4687e58f3ab7d4879197b3ca20f4b54bc3b
Hong Kien Nguyen <nguyenhongkien123tk@gmail.com>
2023-11-04 20:08:53 +1100

    change project display section to normal

--- src/Components/ProjectContent.js
+++ src/Components/ProjectContent.js
@@ -31,18 +31,16 @@ export default function ProjectContent(props){
         initial="hidden"
         animate={control}
         transition={{type:"spring", duration: 2}}
-        ref={component}  className="projectItem">
-            <div style={{display: "flex", alignItems:"center", flexDirection: "row", width: "fit-content", marginLeft:"12.5%"}}>
-                <h2 style={{color: "#333", opacity: 0.3, fontSize: "50px"}}>0{props.id+1}</h2>
-                <h2 style={{marginLeft:"15px"}}>{props.name}</h2>
+        ref={component}  className="projectItem" style={{backgroundImage:`url(${props.src[currIMG]})`, backgroundSize:"cover"}}>
+            {/* <div className="clickToKnow" >
+                <h2 style={{color: props.id===3? "white":"black",display: "flex", justifyContent:"center", flexDirection: "row", width: "100%",textDecoration:"underline", marginTop:"-100px"}}>Click To Know More</h2>
+            </div> */}
+            <div className="projectName" style={{display: "flex", alignItems:"center", justifyContent:"left", flexDirection: "row", width: "100%", marginLeft:"inherit"}}>
+                <h2 style={{color: props.id===3? "white":"#333", opacity: 0.3, fontSize: "50px"}}>0{props.id+1}</h2>
+                <h2 style={{color: props.id===3? "white":"black",marginLeft:"15px"}}>{props.name}</h2>
             </div>    
-                <div className="image--section">
-                    <img src={next} onClick={prevImage} style={{visibility: props.src.length<=1?"hidden": "visible", cursor: "pointer"}} className="prev" alt="Left arrow"/>
-                    <img className="proIMG" onClick={()=>{window.location.href=props.link}} style={{width: "100%", borderRadius: 20, cursor: "pointer"}} alt="Project IMG" src={props.src[currIMG]} />
-                    <img src={next} onClick={nextImage} style={{visibility: props.src.length<=1?"hidden": "visible", cursor: "pointer"}} alt="Right Arrow" className="next" />
-                </div>
-                <p style={{fontSize: "1.2rem",width:"75%", marginLeft:"12.5%"}}><span style={{fontWeight: "600"}}>Skills:</span> {props.skill}</p>
-                <p style={{fontSize: "1.2rem", width:"75%", marginLeft:"12.5%"}}>{props.description}</p>
+
+                
         </motion.div>
     )
 }
