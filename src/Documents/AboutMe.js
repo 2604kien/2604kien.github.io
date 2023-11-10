54ef26c7a5d052f12618d3b194daf0b23dc57612
Hong Kien Nguyen <nguyenhongkien123tk@gmail.com>
2023-11-11 02:34:23 +1100

    adding data in header section

--- src/Documents/AboutMe.js
+++ src/Documents/AboutMe.js
@@ -1,3 +1,3 @@
-export const description=`I am Hong Kien Nguyen. I just graduated Bachelor of Business and Information Technology, 
-majoring in Software Development and Accounting from Monash University. I am not afraid of challenges or taking on 
+export const description=`I am Hong Kien Nguyen. I just graduated Bachelor of Information Technology and Bachelor of Business, 
+specialised in Software Development, Web Development, and Accounting from Monash University. I am not afraid of challenges or taking on 
  full responsibility for the task and I will always try to learn and improve myself day by day.`;
