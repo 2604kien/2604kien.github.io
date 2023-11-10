3bdfd53c30083b9320674efa8ff1aa5443997115
DESKTOP-F3BMJTI\nguye <hngu0053@student.monash.edu>
2023-11-10 17:37:59 +1100

    adding data in project section

--- src/Documents/ImageData.js
+++ src/Documents/ImageData.js
@@ -6,10 +6,10 @@ import yoga1 from "../Components/Images/yogage1.png"
 import yoga2 from "../Components/Images/yogage2.png"
 import yoga3 from "../Components/Images/yogage3.png"
 import yoga from "../Components/Images/yogage.png"
-// import fighting from "../Components/Images/jsFighting.png"
-// import fighting1 from "../Components/Images/jsFighting1.png"
-// import fighting2 from "../Components/Images/jsFighting2.png"
-// import fighting3 from "../Components/Images/jsFighting3.png"
+import fighting from "../Components/Images/jsFighting.png"
+import fighting1 from "../Components/Images/jsFighting1.png"
+import fighting2 from "../Components/Images/jsFighting2.png"
+import fighting3 from "../Components/Images/jsFighting3.png"
 import meme from "../Components/Images/meme.png"
 import meme1 from "../Components/Images/meme1.png"
 import quiz from "../Components/Images/quiz.png"
@@ -20,53 +20,52 @@ import nineman1 from "../Components/Images/9man1.png"
 export const imageData=[{
     name: "Advice Cyborg",
     src: [advice, advice1, advice2, advice3],
-    description: "This is a project that I worked for Partners in Planning company. I worked as a Software Developer in a team of 5 people. The aim of this project is to automate the giving financial advice process to the customers.",
+    description: "This is a project that I worked with a team of 5 people. The aim of this project is to keep track user data, providing financial advice automatically.",
     skill: "PHP, HTML, CSS, Javascript, Git Control",
     duration: "Feb 2023 - Jun 2023",
     link: "https://advicecyborg.com.au/"
 },{
     name: "Yoga Learning Centre",
     src: [yoga, yoga1, yoga2, yoga3],
-    description: "This is a demo project that I work in the University as a preparation to work for a real client. The aim of this project is to create an online system where yoga teacher can post courses and lesson, and student can subscribe and study the course.",
+    description: "The aim of this project is to create an online system where yoga teacher can post courses and lesson, and student can subscribe and study the course.",
     skill: "PHP, HTML, CSS, Javascript, Git Control",
     duration: "",
     link: "#"
 },{
     name: "Website Porfolio",
     src: [profile],
-    description: "This is a project to practice all my Front-end skills to display and make a good looking website with good animation. I build this website from scratch",
+    description: "This project aims to build a portfolio website to describe all my qualifications and help me combine all my front-end skills to develop a static website.",
     skill: "ReactJS, GSAP, Figma, Design, CSS, Git Control",
     duration: "",
     link: "https://github.com/2604kien/2604kien.github.io"},
     {
     name: "Nine Man Moris Game",
     src: [nineman, nineman1],
-    description: "This is a 9 man morris game that is made with Java. This project help me to get better understanding with Object Oriented Programming",
+    description: "The aim of this project is to get a better understanding of Object Oriented Programming.",
     skill: "Java, OOP, Git Control",
     duration: "",
     link: "https://github.com/2604kien/9ManMorris"
 },{
     name: "Meme Generator",
     src: [meme, meme1],
-    description: "This project is when I start learning React and its application. The main purpose of this project is to practice how can fetch data from an API and use external library in React.",
+    description: "The main purpose of this project is to practice how to fetch data from an API and use external libraries in React.",
     skill: "ReactJs, CSS, Javascript, Git Control",
     duration: "",
     link: "https://github.com/2604kien/better-meme-generator"
 },{
     name: "Quiz Generator",
     src: [quiz1, quiz],
-    description: "This project is to practice fetching data from an API and how to manage it such as randomize displayed fetch data (Quiz's Answers)",
+    description: "This project is to practice fetching data from an API and how to manage it such as randomize displayed fetch data (Quiz Answers)",
     skill: "ReactJs, CSS, Javascript, Git Control",
     duration: "",
     link: "https://github.com/2604kien/react_quiz"
+},
+{
+    name: "Js Fighting Game",
+    src: [fighting, fighting1, fighting2, fighting3],
+    description: "This is a project that I did to practice combining my Javascript skills and Object Oriented Programming principles. This project aims to create a fighting game that 2 players can play locally with their keyboard.",
+    skill: "Javascript, HTML, CSS, Git Control",
+    duration: "",
+    link: "https://github.com/2604kien/JsFighting"
 }]
 
-
-// {
-//     name: "Js Fighting Game",
-//     src: [fighting, fighting1, fighting2, fighting3],
-//     description: "This is a project that I did to practice combining my Javascript skills and Object Oriented Programming principles together. The aim of this project is to create a fighting game that 2 players can play locally with their keyboard.",
-//     skill: "Javascript, HTML, CSS, Git Control",
-//     duration: "",
-//     link: "https://github.com/2604kien/JsFighting"
-// },
