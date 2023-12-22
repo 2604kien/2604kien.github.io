02c74593e74edee94f96f408798603472cd1671c
Hong Kien Nguyen <nguyenhongkien123tk@gmail.com>
2023-12-22 17:22:27 +1100

    adjust font and project section

--- src/Components/ProjectContent.js
+++ src/Components/ProjectContent.js
@@ -1,46 +1,51 @@
 import React from "react";
 import "./css/ProjectItem.css";
-import next from "./Images/next.png"
-import {motion, useAnimation, useInView} from "framer-motion"
+import { useAnimation, useInView} from "framer-motion"
+import { useDispatch } from "react-redux";
+import {setIsClick, addToProjectDisplay} from "../reducers/projectSlice";
 export default function ProjectContent(props){
+    const dispatch=useDispatch();
     const [currIMG, setCurrIMG]=React.useState(0);
     let component=React.useRef(null);
     let isInView=useInView(component, { once: false });
     const control=useAnimation();
+    
     React.useEffect(()=>{
         if(isInView){
             control.start("visible");
         }
     },[isInView, control]);
-    const prevImage=()=>{
-        setCurrIMG(prev=>{
-            return prev<=0? props.src.length-1 : prev-1;
-        })
-    }
-    const nextImage=()=>{
-        setCurrIMG(prev=>{
-            return prev>=props.src.length-1? 0 : prev+1;
-        })
+    const handleClick=()=>{
+        const data={
+            description: props.description,
+            skill: props.skill,
+            name: props.name,
+            duration: props.duration,
+            src: props.src,
+            id: props.id,
+            link: props.link
+        }
+        dispatch(addToProjectDisplay(data));
+        dispatch(setIsClick(true))
+
     }
     return (
-        <motion.div 
-        variants={{
-            hidden: {opacity: 0, x: 100},
-            visible: {opacity: 1, x: 0}
-        }}
-        initial="hidden"
-        animate={control}
-        transition={{type:"spring", duration: 2}}
-        ref={component}  className="projectItem" style={{backgroundImage:`url(${props.src[currIMG]})`, backgroundSize:"cover"}}>
+        <div className="projectItem" onClick={handleClick}>
+            <div className="wrapper">
+                <div className="cover-image" style={{backgroundImage:`url(${props.src[currIMG]})`, backgroundSize:"cover"}}></div>
+            </div>
             {/* <div className="clickToKnow" >
                 <h2 style={{color: props.id===3? "white":"black",display: "flex", justifyContent:"center", flexDirection: "row", width: "100%",textDecoration:"underline", marginTop:"-100px"}}>Click To Know More</h2>
             </div> */}
-            <div className="projectName" style={{display: "flex", alignItems:"center", justifyContent:"left", flexDirection: "row", width: "100%", marginLeft:"inherit"}}>
+            <div className="projectName" style={{fontSize: "bold", display: "flex", alignItems:"center", justifyContent:"left", flexDirection: "row", width: "100%", marginLeft:"25px"}}>
                 <h2 style={{color: props.id===3? "white":"#333", opacity: 0.3, fontSize: "50px"}}>0{props.id+1}</h2>
                 <h2 style={{color: props.id===3? "white":"black",marginLeft:"15px"}}>{props.name}</h2>
             </div>    
-
+            <div className="character">
+                <p style={{fontSize: "1rem",width:"75%"}}><span style={{fontWeight: "600"}}>Skills:</span> {props.skill}</p>
+                <p style={{fontSize: "1rem", width:"75%"}}>{props.description}</p>
+            </div>
                 
-        </motion.div>
+        </div>
     )
 }
--- src/Documents/AboutMe.js
+++ src/Documents/AboutMe.js
@@ -1,3 +1,3 @@
-export const description=`I am Hong Kien Nguyen. I just graduated Bachelor of Business and Information Technology, 
+export const description=`I am Hong Kien Nguyen. I graduated Bachelor of Information Technology and Bachelor of Business, 
 majoring in Software Development and Accounting from Monash University. I am not afraid of challenges or taking on 
  full responsibility for the task and I will always try to learn and improve myself day by day.`;
