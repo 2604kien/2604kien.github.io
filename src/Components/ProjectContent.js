ab041558d3501f75f36d4c2f5013e02e9f7ad54d
Hong Kien Nguyen <nguyenhongkien123tk@gmail.com>
2023-12-22 17:30:21 +1100

    fixing color contrast in project section

--- src/Components/ProjectContent.js
+++ src/Components/ProjectContent.js
@@ -38,8 +38,8 @@ export default function ProjectContent(props){
                 <h2 style={{color: props.id===3? "white":"black",display: "flex", justifyContent:"center", flexDirection: "row", width: "100%",textDecoration:"underline", marginTop:"-100px"}}>Click To Know More</h2>
             </div> */}
             <div className="projectName" style={{fontSize: "bold", display: "flex", alignItems:"center", justifyContent:"left", flexDirection: "row", width: "100%", marginLeft:"25px"}}>
-                <h2 style={{color: props.id===3? "white":"#333", opacity: 0.3, fontSize: "50px"}}>0{props.id+1}</h2>
-                <h2 style={{color: props.id===3? "white":"black",marginLeft:"15px"}}>{props.name}</h2>
+                <h2 style={{color: props.id===5 || props.id===8? "white":"#333", opacity: 0.3, fontSize: "50px"}}>0{props.id+1}</h2>
+                <h2 style={{color: props.id===5 || props.id===8? "white":"black",marginLeft:"15px"}}>{props.name}</h2>
             </div>    
             <div className="character">
                 <p style={{fontSize: "1rem",width:"75%"}}><span style={{fontWeight: "600"}}>Skills:</span> {props.skill}</p>
