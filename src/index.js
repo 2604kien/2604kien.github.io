c169b5a9d9db4c332f85a747a39a5070f24ed6e0
Hong Kien Nguyen <nguyenhongkien123tk@gmail.com>
2024-01-12 21:31:06 +1100

    modify project data

--- src/index.js
+++ src/index.js
@@ -4,11 +4,17 @@ import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import {store} from "./store/store"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <Provider store={store}>
-        <App />
+        <BrowserRouter>
+            <Routes>
+                <Route path='/*' element={<App />}/>
+            </Routes>
+        </BrowserRouter>
+
     </Provider>
 );
 reportWebVitals();
