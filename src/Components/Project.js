2144c8ac15e7fd4ac24f1253d665a0afd0cf7ae1
Hong Kien Nguyen <nguyenhongkien123tk@gmail.com>
2024-01-22 09:46:59 +1100

    change about data

--- src/Components/Project.js
+++ src/Components/Project.js
@@ -25,8 +25,8 @@ export default function Project(){
         return ()=>{ctx.revert()}
     },[]);
     const projectElement=imageData.map((data, i)=>{
-        if(i<=5) return<ProjectContent link={data.link} key={i+Math.random()*1000} id={i} src={data.src} description={data.description} duration={data.duration} skill={data.skill} name={data.name}/>;
-        return;
+        if(i<=5) return<ProjectContent link={data.link} key={i+Math.random()*1000} id={i} src={data.src} description={data.description} duration={data.duration} skill={data.skill} name={data.name}/>
+        else return <></>;
 })
     return(
         <div >
